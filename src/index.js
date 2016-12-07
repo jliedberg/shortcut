f815012007017f8a08fa1ac7019997a41ad08c3c
Eskil Fogelström <eskil@eskilfogelstrom.com>
2016-12-08 00:51:09 +0100

    Add normalize, sigrid and custom styles

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
 import getRoutes from './routes'
 
+import 'normalize.css';
+import 'sigrid';
+import 'styles/main.scss';
+
 const initialState = window.__INITIAL_STATE__
 const middleware = routerMiddleware(browserHistory)
 const store = createStore(rootReducer, initialState, applyMiddleware(middleware))
