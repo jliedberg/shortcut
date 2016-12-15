25b0a0b6c89eb04522e2cccc18eee2a513bd9cea
Eskil Fogelström <eskilfogelstrom@gmail.com>
2016-12-15 16:08:52 +0100

    Add shortcut link class for GTM (#9)

--- src/containers/Shortcut/Shortcut.jsx
+++ src/containers/Shortcut/Shortcut.jsx
@@ -7,7 +7,7 @@ export default class Shortcut extends Component {
   render() {
     return (
       <li>
-        <Link to={`/${this.props.clusterIndex}/${this.props.shortcutIndex}`}>
+        <Link to={`/${this.props.clusterIndex}/${this.props.shortcutIndex}`} className="cluster__shortcut">
           <Keys keys={this.props.shortcut.keys}/> - {this.props.shortcut.desc}
         </Link>
       </li>
